49b857e4d358f78b968347393bc7767cf7da6bb6
KazukiOgura <kazuki.ogura@whitecross.co.jp>
2022-04-06 09:50:13 +0900

    詳細画面のデザインを修正

--- backend/resources/views/voice/card.blade.php
+++ backend/resources/views/voice/card.blade.php
@@ -1,26 +1,10 @@
 <div class="card">
-    <header class="card-header">
-        <button class="card-header-icon" aria-label="アイコン">
-            <span class="icon">
-                <i class="far fa-id-card" aria-hidden="true"></i>
-            </span>
-        </button>
-        <button class="card-header-icon" aria-label="アイコン">
-            <span class="icon">
-                <i class="fas fa-angle-down" aria-hidden="true"></i>
-            </span>
-        </button>
-    </header>
     <div class="card-content">
         <div class="content">
-            <strong>コンテンツ</strong>
+            <strong>{{ $voice->title }}</strong>
+            <p>再生時間 {{ $voice->playback_time_minuts }}分</p>
             <br><br>
-            幸せになりたいのなら、欲を捨てなさい。
+            <audio controls src="{{ asset( 'storage/voice/' . $voice->file_name) }}" ></audio>
         </div>
     </div>
-    <footer class="card-footer">
-        <a href="#" class="card-footer-item">フッター1</a>
-        <a href="#" class="card-footer-item">フッター2</a>
-        <a href="#" class="card-footer-item">フッター3</a>
-    </footer>
 </div>
--- backend/resources/views/voice/details.blade.php
+++ backend/resources/views/voice/details.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>{{ $voice->title }}</h1>
-<p>再生時間：{{ $voice->playback_time_minuts . '分' . $voice->playback_time_seconds . '秒' }}</p>
-<audio controls src="{{ asset( 'storage/voice/' . $voice->file_name) }}" ></audio>
+@include('voice.card')
 @endsection
